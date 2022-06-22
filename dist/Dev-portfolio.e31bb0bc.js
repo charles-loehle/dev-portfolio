778dcc9d75a9ae690ac1154e5208f5b3f2ad9787
Charles Loehle <clguitar1@gmail.com>
2022-06-22 10:04:32 -0400

    fixed hero overlay height

--- dist/Dev-portfolio.e31bb0bc.js
+++ dist/Dev-portfolio.e31bb0bc.js
@@ -315,7 +315,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65207" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "65474" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
